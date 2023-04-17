319c3cb8b2b0b960ade3adf3e8c24caf3fa8cd60
Park Su Been <71866185+bymine@users.noreply.github.com>
2023-04-17 10:42:25 +0900

    [#3] refacotr: 클린코드 작성하기 (#4)
    * [#3] refactor: 페이지 공통 UI 처리
    
    - createBrowserRouter를 이용한 router 생성
    - Outlet을 이용한 페이지 공통 UI 처리
    
    * [#3] refactor: isRange 유틸 함수로 지정
    
    - MainPage에서 전역으로 선언된 함수를 uitls폴더에 선언
    
    * [#3] refactor: 문자열 상수화
    
    * [#3] refactor:  mock_data.json 삭제
    
    * [#3] refactor: suspense 간단 적용

---
+++ src/Rourter.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+import {
+  createBrowserRouter,
+  Navigate,
+  RouterProvider,
+} from 'react-router-dom';
+
+import { Layout } from './components';
+import { PAGE } from './constants';
+import { MainPage, ReservationPage } from './pages';
+
+export default function Router() {
+  const router = createBrowserRouter([
+    {
+      element: <Layout />,
+      children: [
+        {
+          path: PAGE.ROOT,
+          element: <Navigate to={PAGE.MAIN} replace />,
+        },
+        {
+          path: PAGE.MAIN,
+          element: <MainPage />,
+        },
+        {
+          path: PAGE.RESERVATION,
+          element: <ReservationPage />,
+        },
+      ],
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
+}
