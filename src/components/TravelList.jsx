2497778a99cacdb2a1281d75b985b40571b37ba5
Park Su Been <71866185+bymine@users.noreply.github.com>
2023-04-17 10:36:39 +0900

    [#3] refactor: suspense 간단 적용

--- src/components/TravelList.jsx
+++ src/components/TravelList.jsx
@@ -1,13 +1,50 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { Grid } from '@chakra-ui/react';
+import { useSelector } from 'react-redux';
 
 import TravelCard from './TravelCard';
+import {
+  selectPriceCategories,
+  selectSpaceCategories,
+} from '../redux/slices/categoriesSlice';
+import { isInRange } from '../utils';
 
-const TravelList = ({ travels }) => {
+const TravelList = () => {
+  const [travels, setTravels] = useState([]);
+
+  const spaceCategories = useSelector(selectSpaceCategories);
+  const priceCategories = useSelector(selectPriceCategories);
+
+  const filteredProductList = travels.filter((product) => {
+    return (
+      spaceCategories.includes(product.spaceCategory) &&
+      isInRange(priceCategories, product.price)
+    );
+  });
+
+  useEffect(() => {
+    async function fetchTravels() {
+      const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+      try {
+        await delay(3000);
+        const response = await fetch(process.env.REACT_APP_API_URL);
+        if (response.ok) {
+          const data = await response.json();
+          setTravels(data);
+        }
+      } catch (error) {
+        throw new Error('Failed to load json data');
+      }
+    }
+
+    fetchTravels();
+  }, []);
+
   return (
     <Grid w="100%" templateColumns="repeat(3,1fr)">
-      {travels.map((element) => (
+      {filteredProductList.map((element) => (
         <TravelCard key={element.idx} travel={element} />
       ))}
     </Grid>
--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,58 +1,20 @@
-import React, { useEffect, useState } from 'react';
+import React, { Suspense } from 'react';
 
 import { Box, Stack } from '@chakra-ui/react';
-import { useSelector } from 'react-redux';
 
 import { TravelFilterBox, TravelList } from '../components';
-import {
-  selectPriceCategories,
-  selectSpaceCategories,
-} from '../redux/slices/categoriesSlice';
-import { isInRange } from '../utils';
 
 const MainPage = () => {
-  const [travels, setTravels] = useState([]);
-
-  const spaceCategories = useSelector(selectSpaceCategories);
-  const priceCategories = useSelector(selectPriceCategories);
-
-  const filteredProductList = travels.filter((product) => {
-    return (
-      spaceCategories.includes(product.spaceCategory) &&
-      isInRange(priceCategories, product.price)
-    );
-  });
-
-  useEffect(() => {
-    async function fetchTravels() {
-      try {
-        const response = await fetch(process.env.REACT_APP_API_URL);
-        if (response.ok) {
-          const data = await response.json();
-          setTravels(data);
-        }
-      } catch (error) {
-        throw new Error('Failed to load json data');
-      }
-    }
-
-    fetchTravels();
-  }, []);
-
-  let content;
-
-  if (travels) {
-    content = (
-      <Stack direction="row">
-        <Box h="auto">
-          <TravelFilterBox />
-        </Box>
-        <TravelList travels={filteredProductList} />
-      </Stack>
-    );
-  }
-
-  return content;
+  return (
+    <Stack direction="row">
+      <Box h="auto">
+        <TravelFilterBox />
+      </Box>
+      <Suspense fallback={<>loading...</>}>
+        <TravelList />
+      </Suspense>
+    </Stack>
+  );
 };
 
 export default MainPage;
